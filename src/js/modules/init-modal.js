1278b251306c51c30e38edfb93d256876a289ff7
antonepifanov <antonepifanov87@gmail.com>
2022-08-22 10:13:06 +0300

    Добавляет модальное окно

--- src/js/modules/init-modal.js
+++ src/js/modules/init-modal.js
@@ -2,12 +2,11 @@
   const orderButtons = document.querySelectorAll("[data-order-button]");
   const modal = document.querySelector(".modal");
   const inputForId = modal.querySelector(".modal__input-hidden");
-  const modalButton = modal.querySelector(".modal__button");
 
   orderButtons.forEach(button => {
     button.addEventListener("click", () => {
       modal.showModal();
       inputForId.value = button.dataset.productId;
-    });
-  })
+    })
+  });
 })();
